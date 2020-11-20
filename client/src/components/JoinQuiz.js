15bc89694630cdf3a3a7fd4610d136e937eb1eac
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-11-21 02:39:20 +0500

    🐛FIX: fIxed global tagnames for styling

--- client/src/components/JoinQuiz.js
+++ client/src/components/JoinQuiz.js
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom'
 
 const JoinQuiz = () => {
     return (
-        <div id = "join-quiz">
+        <div id = "join-quiz-div">
             <label>Enter Quiz Code</label>
             <input type="text"/>
-            <button>Join Quiz</button>
+            <button className = "join-button">Join Quiz</button>
         </div>
     );
 };
