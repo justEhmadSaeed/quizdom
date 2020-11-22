0084ac05319330c4c2b190c5a932811aaa5f986a
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-11-22 19:40:28 +0500

    👌IMPROVE:Added card flexes and all

--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import Appbar from './reuseable-components/Appbar';
-import CreatedQuizCard from './reuseable-components/CreatedQuizCard';
+import React from "react";
+import "./UserDashBoard.css";
+import Appbar from "./reuseable-components/Appbar";
+import CreatedQuizCard from "./reuseable-components/CreatedQuizCard";
 
-const UserDashboard = ({user}) => {
-    return (
-        <div>
-            <Appbar user = {user}/>
-            <CreatedQuizCard title = "Quiz Title" responses = '23'  questions = '50' isOpen ='open'/>
+const UserDashboard = ({ user }) => {
+  return (
+    <div>
+      <Appbar user={user} />
+      <div className="dash-body">
+        <div className="heading">
+          <div>Created </div>
+          <div className="line"></div>
         </div>
-    );
+        <div className="card-holder">
+          <CreatedQuizCard
+            title="Quiz Title"
+            responses="23"
+            questions="50"
+            isOpen="open"
+          />
+          <CreatedQuizCard
+            title="Quiz Title"
+            responses="23"
+            questions="50"
+            isOpen="open"
+          />
+          <CreatedQuizCard
+            title="Quiz Title"
+            responses="23"
+            questions="50"
+            isOpen="open"
+          />
+          <CreatedQuizCard
+            title="Quiz Title"
+            responses="23"
+            questions="50"
+            isOpen="open"
+          />
+          <CreatedQuizCard
+            title="Quiz Title"
+            responses="23"
+            questions="50"
+            isOpen="open"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserDashboard;
--- client/src/components/reuseable-components/CreatedQuizCard.js
+++ client/src/components/reuseable-components/CreatedQuizCard.js
@@ -5,7 +5,7 @@ const CreatedQuizCard = ({ title, responses, questions, isOpen }) => {
   return (
     <div className="quiz-card">
       <div id="quiz-title">{title}</div>
-      <div id="horizontal-line" />
+      <div id="horizontal-line"></div>
       <div id="row">
         <div id="responses">Responses : {responses}</div>
         <div id="questions">Questions : {questions}</div>
