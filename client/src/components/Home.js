6befa5e982100158b84f63b49cca10889443a8d6
iqrafatimame <candy.iqo@gmail.com>
2020-11-22 13:09:13 -0800

    📦 NEW: Added Description div

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,9 +5,15 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div id="Home">
-      <span id="logo">
-        <b>Quiz</b>dom
-      </span>
+      <div id="logo">
+        <div id="logo-name"><b style={{fontweight : 600}}>Quiz</b>dom</div>
+        <div id="description">
+          Now create and join quiz at a single platform.You can 
+          create trivia quizzes, personality test, polls and survays.
+          Share out your quiz with your students with a unique code. 
+        </div>
+      </div>
+      
       <span id="login-card">
         <label><b>Q</b></label>
         <Link to="/new-user-dashboard">
