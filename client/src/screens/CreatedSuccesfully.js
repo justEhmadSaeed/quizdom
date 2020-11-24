9b2b07dbbf4574cf263bcbcba9d2e985b6a4d453
Ehmad Saeed <justehmadsaeed@gmail.com>
2020-11-25 02:49:08 +0500

    📦 NEW: Link dashboard

--- client/src/screens/CreatedSuccesfully.js
+++ client/src/screens/CreatedSuccesfully.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './CreatedSuccesfully.css';
-const CreatedSuccesfully = ({user}) => {
-  
-  return( 
-    <div id="created-quiz">
-      <div id = "created-quiz-div">
-      <div id="message">
-        <b>
-          Quiz Created Succefully</b>
-        <p>Share the follwong code with your students</p>
-        </div>
-      <input id="copy-code" type="text" placeholder="AsdqweRfgZ" />
-      <button className = "copy-btn" >copy</button>
-      </div>
-  </div>
-  );
-}
-export default CreatedSuccesfully
+import "./CreatedSuccesfully.css";
+const CreatedSuccesfully = ({ user }) => {
+	return (
+		<div id="created-quiz">
+			<div id="created-quiz-div">
+				<div id="message">
+					<b>Quiz Created Succefully</b>
+					<p>Share the follwong code with your students</p>
+				</div>
+				<input id="copy-code" type="text" placeholder="AsdqweRfgZ" />
+				<button className="copy-btn">copy</button>
+				<Link to={user.id + "/dashboard"}>
+					<button className="copy-btn">Dashboard</button>
+				</Link>
+			</div>
+		</div>
+	);
+};
+export default CreatedSuccesfully;
