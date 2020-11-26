3da2f707cd964b11f35444fc87afed558ed29117
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-11-26 13:35:45 +0500

    🎨ADD : added style to input copy

--- client/src/screens/CreatedSuccesfully.js
+++ client/src/screens/CreatedSuccesfully.js
@@ -5,7 +5,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const CreatedSuccesfully = ({ user }) => {
   const [Code, setCode] = useState("AsdqweRfgZ");
-  const [Copy, setCopy] = useState("Copy Code");
+  const [Copy, setCopy] = useState("copy");
   return (
     <div id="created-quiz">
       <div id="created-quiz-div">
@@ -13,9 +13,9 @@ const CreatedSuccesfully = ({ user }) => {
           <b>Quiz Created Succefully</b>
           <p>Share the follwong code with your students</p>
         </div>
-        <input id="copy-code" type="text" value={Code} />
-        <CopyToClipboard text={Code} onCopy={()=>{setCopy("Code Copied!")}}>
-          <button className="copy-btn">{Copy}</button>
+        <input id={Copy} type="text" value={Code} />
+        <CopyToClipboard text={Code} onCopy={()=>{setCopy("copied")}}>
+          <button className="copy-btn">{Copy=== "copy"? "Copy Code":"Code Copied!"}</button>
         </CopyToClipboard>
         <Link to={user.id + "/dashboard"}>
           <button className="copy-btn">Dashboard</button>
