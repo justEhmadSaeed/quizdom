888a8b34f4ec482d2fd29b0b277d50ddfb1c995d
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-11-26 13:13:47 +0500

    📦 ADD : Add copy to clipboard functionality

--- client/src/screens/CreatedSuccesfully.js
+++ client/src/screens/CreatedSuccesfully.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./CreatedSuccesfully.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 const CreatedSuccesfully = ({ user }) => {
-	return (
-		<div id="created-quiz">
-			<div id="created-quiz-div">
-				<div id="message">
-					<b>Quiz Created Succefully</b>
-					<p>Share the follwong code with your students</p>
-				</div>
-				<input id="copy-code" type="text" placeholder="AsdqweRfgZ" />
-				<button className="copy-btn">copy</button>
-				<Link to={user.id + "/dashboard"}>
-					<button className="copy-btn">Dashboard</button>
-				</Link>
-			</div>
-		</div>
-	);
+  const [Code, setCode] = useState("AsdqweRfgZ");
+  const [Copy, setCopy] = useState("Copy Code");
+  return (
+    <div id="created-quiz">
+      <div id="created-quiz-div">
+        <div id="message">
+          <b>Quiz Created Succefully</b>
+          <p>Share the follwong code with your students</p>
+        </div>
+        <input id="copy-code" type="text" value={Code} />
+        <CopyToClipboard text={Code} onCopy={()=>{setCopy("Code Copied!")}}>
+          <button className="copy-btn">{Copy}</button>
+        </CopyToClipboard>
+        <Link to={user.id + "/dashboard"}>
+          <button className="copy-btn">Dashboard</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default CreatedSuccesfully;
