d93ea90b24c5b48c2252c37a8b7de89facb00443
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-11-26 13:43:09 +0500

    📦ADD:Added attempted quiizzes in dashboard

--- client/src/screens/UserDashboard.js
+++ client/src/screens/UserDashboard.js
@@ -10,61 +10,117 @@ const UserDashboard = ({ user }) => {
         <Appbar user={user} />
       </div>
       <div className="dash-body">
-        <div className="heading">
-          <div>Created </div>
-          <div className="line"></div>
+        <div className="quizzes">
+          <div className="heading">
+            <div>Created </div>
+            <div className="line"></div>
+          </div>
+          <div className="card-holder">
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 1",
+                responses: "23",
+                questions: "50",
+                isOpen: "open",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 2",
+                responses: "23",
+                questions: "50",
+                isOpen: "closed",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 3",
+                responses: "23",
+                questions: "50",
+                isOpen: "open",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 4",
+                responses: "23",
+                questions: "50",
+                isOpen: "closed",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 5",
+                responses: "23",
+                questions: "50",
+                isOpen: "open",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 6",
+                responses: "23",
+                questions: "50",
+                isOpen: "closed",
+              }}
+            />
+          </div>
         </div>
-        <div className="card-holder">
-        <CreatedQuizCard
-            quiz={
-              {title : "Quiz 1",
-              responses : "23",
-              questions : "50",
-              isOpen : "open"}
-            }
-          />
-          <CreatedQuizCard
-            quiz={
-              {title : "Quiz 2",
-              responses : "23",
-              questions : "50",
-              isOpen : "closed"}
-            }
-          />
-          <CreatedQuizCard
-            quiz={
-              {title : "Quiz 3",
-              responses : "23",
-              questions : "50",
-              isOpen : "open"}
-            }
-          />
-          <CreatedQuizCard
-            quiz={
-              {title : "Quiz 4",
-              responses : "23",
-              questions : "50",
-              isOpen : "closed"}
-            }
-          />
-          <CreatedQuizCard
-            quiz={
-              {title : "Quiz 5",
-              responses : "23",
-              questions : "50",
-              isOpen : "open"}
-            }
-          />
-          <CreatedQuizCard
-            quiz={
-              {title : "Quiz 6",
-              responses : "23",
-              questions : "50",
-              isOpen : "closed"}
-            }
-          />
-          
-          
+        <div className="quizzes">
+          <div className="heading">
+            <div>Attempted </div>
+            <div className="line"></div>
+          </div>
+          <div className="card-holder">
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 1",
+                responses: "23",
+                questions: "50",
+                isOpen: "open",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 2",
+                responses: "23",
+                questions: "50",
+                isOpen: "closed",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 3",
+                responses: "23",
+                questions: "50",
+                isOpen: "open",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 4",
+                responses: "23",
+                questions: "50",
+                isOpen: "closed",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 5",
+                responses: "23",
+                questions: "50",
+                isOpen: "open",
+              }}
+            />
+            <CreatedQuizCard
+              quiz={{
+                title: "Quiz 6",
+                responses: "23",
+                questions: "50",
+                isOpen: "closed",
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
