b8eca96f092e0e4a128f2fe3ac2920c692b9be6b
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-11-27 23:36:54 +0500

    📦ADD:added create question card

---
+++ client/src/components/AddQuestionCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CreatedQuizCard.css';
+
+const AddQuestionCard = ({qNo}) => {
+    return (
+        <div className = "questionCard">
+
+        </div>
+    );
+};
+
+export default AddQuestionCard;
