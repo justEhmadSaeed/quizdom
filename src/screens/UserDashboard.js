8d52202f8d344a3c0654c314ede12f72e7b7dd29
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-12-14 12:05:44 +0500

    🎨FIX: Quiz card alignment

--- src/screens/UserDashboard.js
+++ src/screens/UserDashboard.js
@@ -4,103 +4,105 @@ import Appbar from "../components/Appbar";
 import CreatedQuizCard from "../components/CreatedQuizCard";
 
 const UserDashboard = ({ user }) => {
-	return (
-		<div>
-			<div className="appheader">
-				<Appbar user={user} />
-			</div>
-			<div className="dash-body">
-				<div className="quizzes">
-					<div className="heading">
-						<div>Created </div>
-						<div className="line"></div>
-					</div>
-					<div className="card-holder">
-						<CreatedQuizCard
-							title="Quiz 1"
-							responses="23"
-							questions="50"
-							isOpen={true}
-						/>
-						<CreatedQuizCard
-							title="Quiz 2"
-							responses="23"
-							questions="50"
-							isOpen={false}
-						/>
-						<CreatedQuizCard
-							title="Quiz 3"
-							responses="23"
-							questions="50"
-							isOpen={true}
-						/>
-						<CreatedQuizCard
-							title="Quiz 4"
-							responses="23"
-							questions="50"
-							isOpen={false}
-						/>
-						<CreatedQuizCard
-							title="Quiz 5"
-							responses="23"
-							questions="50"
-							isOpen={true}
-						/>
-						<CreatedQuizCard
-							title="Quiz 6"
-							responses="23"
-							questions="50"
-							isOpen={false}
-						/>
-					</div>
-				</div>
-				<div className="quizzes">
-					<div className="heading">
-						<div>Attempted </div>
-						<div className="line"></div>
-					</div>
-					<div className="card-holder">
-						<CreatedQuizCard
-							title="Quiz 1"
-							responses="23"
-							questions="50"
-							isOpen={true}
-						/>
-						<CreatedQuizCard
-							title="Quiz 2"
-							responses="23"
-							questions="50"
-							isOpen={false}
-						/>
-						<CreatedQuizCard
-							title="Quiz 3"
-							responses="23"
-							questions="50"
-							isOpen={true}
-						/>
-						<CreatedQuizCard
-							title="Quiz 4"
-							responses="23"
-							questions="50"
-							isOpen={false}
-						/>
-						<CreatedQuizCard
-							title="Quiz 5"
-							responses="23"
-							questions="50"
-							isOpen={true}
-						/>
-						<CreatedQuizCard
-							title="Quiz 6"
-							responses="23"
-							questions="50"
-							isOpen={false}
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div id="dashboard">
+      <div className="appheader">
+        <Appbar user={user} />
+      </div>
+      <div className="dash-body">
+        <div className="quizzes">
+          <div className="heading">
+            <div className="line-left" />
+            <div>Created </div>
+            <div className="line" />
+          </div>
+          <div className="card-holder">
+            <CreatedQuizCard
+              title="Quiz 1"
+              responses="23"
+              questions="50"
+              isOpen={true}
+            />
+            <CreatedQuizCard
+              title="Quiz 2"
+              responses="23"
+              questions="50"
+              isOpen={false}
+            />
+            <CreatedQuizCard
+              title="Quiz 3"
+              responses="23"
+              questions="50"
+              isOpen={true}
+            />
+            <CreatedQuizCard
+              title="Quiz 4"
+              responses="23"
+              questions="50"
+              isOpen={false}
+            />
+            <CreatedQuizCard
+              title="Quiz 5"
+              responses="23"
+              questions="50"
+              isOpen={true}
+            />
+            <CreatedQuizCard
+              title="Quiz 6"
+              responses="23"
+              questions="50"
+              isOpen={false}
+            />
+          </div>
+        </div>
+        <div className="quizzes">
+          <div className="heading">
+            <div className="line-left" />
+            <div>Attempted </div>
+            <div className="line" />
+          </div>
+          <div className="card-holder">
+            <CreatedQuizCard
+              title="Quiz 1"
+              responses="23"
+              questions="50"
+              isOpen={true}
+            />
+            <CreatedQuizCard
+              title="Quiz 2"
+              responses="23"
+              questions="50"
+              isOpen={false}
+            />
+            <CreatedQuizCard
+              title="Quiz 3"
+              responses="23"
+              questions="50"
+              isOpen={true}
+            />
+            <CreatedQuizCard
+              title="Quiz 4"
+              responses="23"
+              questions="50"
+              isOpen={false}
+            />
+            <CreatedQuizCard
+              title="Quiz 5"
+              responses="23"
+              questions="50"
+              isOpen={true}
+            />
+            <CreatedQuizCard
+              title="Quiz 6"
+              responses="23"
+              questions="50"
+              isOpen={false}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserDashboard;
