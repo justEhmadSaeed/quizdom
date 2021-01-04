c2a0baa564064f90f84b7d210d51bc8ffd64b2f8
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-01-04 23:14:22 +0500

    🐛 FIX: Solve Merge Conflicts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-import { Switch, Route } from "react-router-dom";
-import React, { useState } from "react";
-import firebaseConfig from "./firebase/firebaseConfig.js";
-import firebase from "firebase/app";
-import "firebase/auth";
-
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-=======
 import { Switch, Route, Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import firebase from "./firebase/firebase";
 import { StyledFirebaseAuth } from "react-firebaseui";
 
->>>>>>> d122dd3b86f6493a8bada3fe5479576960404f00
 // Stylesheet
 import "./App.css";
 
@@ -26,34 +16,6 @@ import CreatedSuccesfully from "./screens/CreatedSuccesfully";
 
 const App = () => {
   const [User, setUser] = useState({ name: "", id: "" });
-<<<<<<< HEAD
-  const [isLoggedIn, setisLoggedIn] = useState(false);
-  
-  
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Home user={User} />
-        </Route>
-
-        <Route path="/new-user-dashboard">
-          <OneTimeDashBoard user={User} />
-        </Route>
-        <Route path={User.id + "/dashboard"}>
-          <UserDashboard user={User} />
-        </Route>
-        <Route path={User.id + "/create-quiz"}>
-          <CreateQuiz user={User} />
-        </Route>
-        <Route path={User.id + "/created-succesfully"}>
-          <CreatedSuccesfully user={User} />
-        </Route>
-        <Route path={User.id + "/join-quiz"}>
-          <JoinQuiz user={User} />
-        </Route>
-      </Switch>
-=======
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   var uiConfig = {
     signInflow: "popup",
@@ -107,7 +69,6 @@ const App = () => {
           </Route>
         </Switch>
       )}
->>>>>>> d122dd3b86f6493a8bada3fe5479576960404f00
     </div>
   );
 };
