8b322808cc2eb54bddebfa0f982ded743f20cb11
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-01-10 22:58:49 +0500

    👌 IMPROVE: Implement Firebase UI in Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { Switch, Route, Link } from "react-router-dom"
-import React, { useState, useEffect } from "react"
+import { Switch, Route } from "react-router-dom"
+import React, { useState } from "react"
 import firebase from "./firebase/firebase"
-import { StyledFirebaseAuth } from "react-firebaseui"
 
 // Stylesheet
 import "./App.css"
@@ -15,42 +14,12 @@ import UserDashboard from "./screens/UserDashboard"
 import CreatedSuccesfully from "./screens/CreatedSuccesfully"
 
 const App = () => {
-	const [User, setUser] = useState({ name: "", id: "" })
-	const [isLoggedIn, setIsLoggedIn] = useState(false)
-	var uiConfig = {
-		signInflow: "popup",
-		signInOptions: [
-			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-			firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-			firebase.auth.EmailAuthProvider.PROVIDER_ID,
-		],
-		callbacks: {
-			signInSuccessWithAuthResult: () => false,
-		},
-	}
-	useEffect(() => {
-		firebase.auth().onAuthStateChanged((user) => {
-			setIsLoggedIn(!!user)
-			if (user) {
-				setUser({
-					id: firebase.auth().currentUser.uid,
-					name: firebase.auth().currentUser.displayName,
-				})
-				console.log("Firebase User - ", user)
-			}
-		})
-	}, [])
+	const [User, setUser] = useState({})
 
 	return (
 		<div className="App">
 			{!firebase.auth().currentUser ? (
-				<div className="Home">
-					<StyledFirebaseAuth
-						borderRadius="40px"
-						uiConfig={uiConfig}
-						firebaseAuth={firebase.auth()}
-					/>
-				</div>
+				<Home setUser={setUser} />
 			) : (
 				<Switch>
 					<Route exact path="/">
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,60 +1,62 @@
-import React, { useState } from "react";
-import "./Home.css";
-import { Link } from "react-router-dom";
-import UserDashboard from "./UserDashboard";
-import { StyledFirebaseAuth } from "react-firebaseui";
-import firebase from "../firebase/firebase";
-import { useEffect } from "react";
+import React, { useEffect } from "react"
+import "./Home.css"
+import { StyledFirebaseAuth } from "react-firebaseui"
+import firebase from "../firebase/firebase"
 
-const Home = (props) => {
-  const [homeUser, setHomeUser] = useState({ name: "", id: "" });
-  var uiConfig = {
-    signInflow: "popup",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false,
-    },
-  };
-  // useEffect(() => {
-  //   firebase.auth().onAuthStateChanged((user) => {
-  //     props.setIsLoggedIn(!!user);
-  //     setHomeUser(firebase.auth().currentUser);
-  //   });
-  // }, [homeUser]);
-  return (
-    <div id="Home">
-      <div id="logo">
-        <div id="logo-name">
-          <b>Quiz</b>dom
-        </div>
-        <div id="description">
-          Now create and join quiz at a single platform.You can create trivia
-          quizzes, personality test, polls and survays. Share out your quiz with
-          your students with a unique code.
-        </div>
-      </div>
+const Home = ({ setUser }) => {
+	var uiConfig = {
+		signInflow: "popup",
+		signInOptions: [
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+			firebase.auth.EmailAuthProvider.PROVIDER_ID,
+		],
+		callbacks: {
+			signInSuccessWithAuthResult: () => false,
+		},
+	}
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged((user) => {
+			// setIsLoggedIn(!!user)
+			if (user) {
+				setUser({
+					id: firebase.auth().currentUser.uid,
+					name: firebase.auth().currentUser.displayName,
+				})
+				console.log("User Logged In")
+			} else {
+				console.log("User Signed Out")
+				setUser({})
+			}
+		})
+	}, [setUser])
+	return (
+		<div id="Home">
+			<div id="logo">
+				<div id="logo-name">
+					<b>Quiz</b>dom
+				</div>
+				<div id="description">
+					Now create and join quiz at a single platform.You can create trivia
+					quizzes, personality test, polls and survays. Share out your quiz with
+					your students with a unique code.
+				</div>
+			</div>
 
-      <span id="login-card">
-        <label>
-          <b>Q</b>
-        </label>
-        <Link to="/new-user-dashboard">
-          <button className="signIn-btn">Sign-in with Google</button>
-        </Link>
-        <StyledFirebaseAuth
-          borderRadius="40px"
-          uiConfig={uiConfig}
-          firebaseAuth={firebase.auth()}
-        />
-        <button className="signIn-btn">Sign-in with Facebook</button>
-        <button className="signIn-btn">Sign-in with Email</button>
-      </span>
-    </div>
-  );
-};
+			<span id="login-card">
+				<label>
+					<b>Q</b>
+				</label>
+				<div className="Home">
+					<StyledFirebaseAuth
+						borderRadius="40px"
+						uiConfig={uiConfig}
+						firebaseAuth={firebase.auth()}
+					/>
+				</div>
+			</span>
+		</div>
+	)
+}
 
-export default Home;
+export default Home
