15d352438ae05655693486c71c92aa6fa3cc80e6
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-01-10 22:59:05 +0500

    📦 NEW: Sign Out From Appbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,74 +1,74 @@
-import React, {useState} from 'react'
-import { Link } from "react-router-dom";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import "./Sidebar.css";
-import "./colors.css";
-import { IconContext } from 'react-icons';
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import * as FaIcons from "react-icons/fa"
+import * as AiIcons from "react-icons/ai"
+import "./Sidebar.css"
+import "./colors.css"
+import { IconContext } from "react-icons"
 import firebase from "../firebase/firebase"
 
 function Sidebar() {
+	const SidedbarData = [
+		{
+			title: "Dashboard",
+			path: "/dashboard",
+			icon: <AiIcons.AiFillDashboard />,
+			CName: "nav-text",
+		},
+		{
+			title: "Join Quiz",
+			path: "/join-quiz",
+			icon: <FaIcons.FaPencilAlt />,
+			CName: "nav-text",
+		},
+		{
+			title: "Create Quiz",
+			path: "/create-quiz",
+			icon: <AiIcons.AiFillFolderAdd />,
+			CName: "nav-text",
+		},
+	]
+	const [sidebar, setSidebar] = useState(false)
+	const showSidebar = () => setSidebar(!sidebar)
 
-  const SidedbarData =[
-    {
-      title: 'Dashboard',
-      path: '/dashboard',
-      icon: <AiIcons.AiFillDashboard />,
-      CName: 'nav-text',
-    },
-    {
-      title: 'Join Quiz',
-      path: '/',
-      icon: <FaIcons.FaPencilAlt />,
-      CName: 'nav-text',
-    },
-    {
-      title: 'Create Quiz',
-      path: '/create-quiz',
-      icon: <AiIcons.AiFillFolderAdd />,
-      CName: 'nav-text',
-    },
-    {
-      title: 'Logout',
-      path: '/',
-      icon: <FaIcons.FaUser />,
-      CName: 'nav-text',
-    },
-  ]
-  const [sidebar, setSidebar] = useState(false)
-  const showSidebar = () => setSidebar(!sidebar)
-
-  return (
-    <>
-    <IconContext.Provider value={{color: 'white'}}>
-      <div className="navbar">
-        <Link to="#" className="menu-bars">
-          <FaIcons.FaBars onClick={showSidebar} />
-        </Link>
-        <Link to="/new-user-dashboard" className = "home">
-          <b>Quiz</b>dom
-      </Link>
-      </div>
-      <nav className={sidebar ? 'nav-menu active': 'nav-menu'}>
-        <ul className='nav-menu-items' onClick={showSidebar}>
-          <li className='navbar-toggle'>
-              <AiIcons.AiOutlineClose />
-          </li>
-          {SidedbarData.map((item, index) => {
-            return ( 
-              <li key={index} className={item.CName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span className="nav-item-title">{item.title}</span>
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
-      </IconContext.Provider>
-    </>
-  )
+	return (
+		<>
+			<IconContext.Provider value={{ color: "white" }}>
+				<div className="navbar">
+					<Link to="#" className="menu-bars">
+						<FaIcons.FaBars onClick={showSidebar} />
+					</Link>
+					<Link to="/" className="home">
+						<b>Quiz</b>dom
+					</Link>
+				</div>
+				<nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+					<ul className="nav-menu-items" onClick={showSidebar}>
+						<li className="navbar-toggle">
+							<AiIcons.AiOutlineClose />
+						</li>
+						{SidedbarData.map((item, index) => {
+							return (
+								<li key={index} className={item.CName}>
+									<Link to={item.path}>
+										{item.icon}
+										<span className="nav-item-title">{item.title}</span>
+									</Link>
+								</li>
+							)
+            })}
+            {/* Sign Out Button */}
+						<li className="nav-text">
+							<button onClick={() => firebase.auth().signOut()}>
+								<FaIcons.FaSignOutAlt />
+								<span className="nav-item-title">{"Sign Out"}</span>
+							</button>
+						</li>
+					</ul>
+				</nav>
+			</IconContext.Provider>
+		</>
+	)
 }
 
 export default Sidebar
