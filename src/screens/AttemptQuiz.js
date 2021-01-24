217b600a7a94577ba2595e58fe82207bf98f764a
Zaeema Anwar <thezaeemaanwar@gmail.com>
2021-01-24 22:36:03 +0500

    👌 IMPROVE : A lot of stuff

--- src/screens/AttemptQuiz.js
+++ src/screens/AttemptQuiz.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Appbar from "../components/Appbar";
-//import "./AttemptQuiz.css";
 
 const AttemptQuiz = (props) => {
+  const [attemptedQuestions, setAttemptedQuestions] = useState(() => {
+    const temp = [];
+    props.questions.forEach((ques) => {
+      temp.push({
+        title: ques.title,
+        type: ques.optionType,
+        selectedOptions: [],
+      });
+    });
+    return temp;
+  });
+
+  const handleOptionSelect = (e, option, index) => {
+    const temp = [...attemptedQuestions];
+    const options = temp[index].selectedOptions;
+
+    if (!options.includes(option) && e.target.checked) {
+      options.push(option);
+    }
+    if (options.includes(option) && !e.target.checked) {
+      const i = options.indexOf(option);
+      options.splice(i, 1);
+    }
+
+    temp[index].selectedOptions = options;
+    setAttemptedQuestions(temp);
+  };
+
+  console.log("selected options : ", attemptedQuestions);
+  const submitQuiz = () => {
+    // send attemped Questions to backend
+  };
   return (
     <div id="main-body">
       <div className="appheader">
@@ -10,7 +41,7 @@ const AttemptQuiz = (props) => {
       </div>
       <div id="create-quiz-body">
         <div className="quiz-header">
-          <label id="quiz-title">{props.quizTitle}</label>
+          <h2>{props.quizTitle}</h2>
         </div>
         {props.questions.map((question, index) => (
           <div className="questionCard" key={index}>
@@ -19,17 +50,29 @@ const AttemptQuiz = (props) => {
               {question.options.map((option, ind) => (
                 <div className="option" key={ind}>
                   {question.optionType === "oneOp" ? (
-                    <input className="radio-in" type="radio" name="oneOp" />
+                    <input
+                      type="radio"
+                      name="option"
+                      onChange={(e) =>
+                        handleOptionSelect(e, option.text, index)
+                      }
+                    />
                   ) : (
-                    <input className="check-in" type="checkbox" name="mulOp" />
+                    <input
+                      type="checkbox"
+                      name="option"
+                      onChange={(e) =>
+                        handleOptionSelect(e, option.text, index)
+                      }
+                    />
                   )}
-                  <label className="option-text">{option.text}</label>
+                  <label>{option.text}</label>
                 </div>
               ))}
             </div>
           </div>
         ))}
-        <button>Submit</button>
+        <button onClick={submitQuiz}>Submit</button>
       </div>
     </div>
   );
--- src/screens/CreateQuiz.js
+++ src/screens/CreateQuiz.js
@@ -7,68 +7,61 @@ import QuestionsTable from "../components/QuestionsTable";
 import { Button, Switch } from "@material-ui/core";
 
 const CreateQuiz = ({ user }) => {
-	const [questionArray, setQuestionArray] = useState([]);
-	const [title, setTitle] = useState("");
-	const [accesss, setAccesss] = useState(false);
+  const [questionArray, setQuestionArray] = useState([]);
+  const [title, setTitle] = useState("");
+  const [accesss, setAccesss] = useState(false);
 
-	const addQuestionHandle = (title, optionType, options) => {
-		const arr = [...questionArray];
-		arr.push({ title, optionType, options });
-		setQuestionArray(arr);
-	};
-	console.table(questionArray);
-	return (
-		<div id="main-body">
-			<div className="appheader">
-				<Appbar user={user} />
-			</div>
-			<div id="create-quiz-body">
-				<div className="quiz-header">
-					<input
-						value={title}
-						onChange={(e) => setTitle(e.target.value)}
-						id="quiz-title"
-						type="text"
-						placeholder="Untitled Quiz"
-					/>
-				</div>
-				<div className="controls">
-					<AddQuestionModal addQuestionHandle={addQuestionHandle} />
-					<Button color="secondary" variant="outlined">
-						Responses 0
-					</Button>
-					<div className="switch">
-						<Switch
-							checked={accesss}
-							onChange={(e) => setAccesss(e.target.checked)}
-							color="secondary"
-							name="access"
-						/>
-						<h4>Access Control</h4>
-					</div>
-				</div>
-			</div>
-			<QuestionsTable
-				questionArray={questionArray}
-				setQuestionArray={setQuestionArray}
-			/>
-			{title.length && questionArray.length ? (
-				<Link className="link" to="/created-succesfully">
-					<Button
-						onClick={() => console.log({ title, accesss, questionArray })}
-						variant="contained"
-						color="secondary"
-					>
-						Create Quiz
-					</Button>
-				</Link>
-			) : (
-				<Button variant="contained" color="secondary" disabled>
-					Create Quiz
-				</Button>
-			)}
-		</div>
-	);
+  const addQuestionHandle = (title, optionType, options) => {
+    const arr = [...questionArray];
+    arr.push({ title, optionType, options });
+    setQuestionArray(arr);
+  };
+  console.table(questionArray);
+  return (
+    <div id="main-body">
+      <div className="appheader">
+        <Appbar user={user} />
+      </div>
+      <div id="create-quiz-body">
+        <div className="quiz-header">
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            id="quiz-title"
+            type="text"
+            placeholder="Untitled Quiz"
+          />
+        </div>
+        <div className="controls">
+          <AddQuestionModal addQuestionHandle={addQuestionHandle} />
+          <button className="outlined">Responses 0</button>
+          <div className="switch">
+            <Switch
+              checked={accesss}
+              onChange={(e) => setAccesss(e.target.checked)}
+              color="secondary"
+              name="access"
+            />
+            <h4>Access Control</h4>
+          </div>
+        </div>
+      </div>
+      <QuestionsTable
+        questionArray={questionArray}
+        setQuestionArray={setQuestionArray}
+      />
+      {title.length && questionArray.length ? (
+        <Link className="link" to="/created-succesfully">
+          <button
+            onClick={() => console.log({ title, accesss, questionArray })}
+          >Create Quiz
+          </button>
+        </Link>
+      ) : (
+        <button>Create Quiz</button>
+      )}
+    </div>
+  );
 };
 
 export default CreateQuiz;
