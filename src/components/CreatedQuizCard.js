9d3f7f977b838a692cc98c077f636d22993f19b5
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-02-23 20:23:23 +0500

    👌 IMPROVE: Quiz Response Link for DB

--- src/components/CreatedQuizCard.js
+++ src/components/CreatedQuizCard.js
@@ -1,7 +1,8 @@
-import { IconButton } from "@material-ui/core"
-import { EditRounded } from "@material-ui/icons"
-import React from "react"
-import "./QuizCard.css"
+import { IconButton } from '@material-ui/core'
+import { EditRounded } from '@material-ui/icons'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './QuizCard.css'
 
 const CreatedQuizCard = ({
 	title,
@@ -13,18 +14,22 @@ const CreatedQuizCard = ({
 	setEditQuiz,
 }) => {
 	return (
-		<div className="quiz-card">
+		<div className='quiz-card'>
 			<div>
-				<h1 id="created-quiz-title">{title}</h1>
-				<p className="card-code">Code: {code}</p>
+				<h1 id='created-quiz-title'>{title}</h1>
+				<p className='card-code'>Code: {code}</p>
 			</div>
-			<div id="horizontal-line"></div>
-			<div id="row">
-				<div id="responses">Responses : {responses}</div>
-				<div id="questions">Questions : {questions}</div>
+			<div id='horizontal-line'></div>
+			<div id='row'>
+				<div id='responses'>
+					<Link to={`/responses/${code}`} style={{ textDecoration: 'none' }}>
+						Responses : {responses}
+					</Link>
+				</div>
+				<div id='questions'>Questions : {questions}</div>
 			</div>
-			<div className="bottom-bar">
-				{isOpen ? <div id="open">open</div> : <div id="closed">closed</div>}
+			<div className='bottom-bar'>
+				{isOpen ? <div id='open'>open</div> : <div id='closed'>closed</div>}
 				<IconButton onClick={() => setEditQuiz([index])}>
 					<EditRounded />
 				</IconButton>
--- src/screens/UserDashboard.js
+++ src/screens/UserDashboard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import './UserDashBoard.css'
 import CreatedQuizCard from '../components/CreatedQuizCard'
 import JoinedQuizCard from '../components/JoinedQuizCard'
@@ -7,118 +6,112 @@ import LoadingScreen from './LoadingScreen'
 import CreateQuiz from './CreateQuiz'
 
 const UserDashboard = ({ user }) => {
-    const [createdQuizzes, setCreatedQuizzes] = useState([])
-    const [attemptedQuizzes, setAttemptedQuizzes] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [editQuiz, setEditQuiz] = useState([])
-    useEffect(() => {
-        const fetchQuizData = async () => {
-            const results = await fetch(`/API/users/${user.uid}`)
-            const quizData = await results.json()
-            if (quizData.createdQuiz) setCreatedQuizzes(quizData.createdQuiz)
-            if (quizData.attemptedQuiz)
-                setAttemptedQuizzes(quizData.attemptedQuiz)
-            setLoading(false)
-        }
-        if (user) fetchQuizData()
-    }, [user])
+	const [createdQuizzes, setCreatedQuizzes] = useState([])
+	const [attemptedQuizzes, setAttemptedQuizzes] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [editQuiz, setEditQuiz] = useState([])
+	useEffect(() => {
+		const fetchQuizData = async () => {
+			const results = await fetch(`/API/users/${user.uid}`)
+			const quizData = await results.json()
+			if (quizData.createdQuiz) setCreatedQuizzes(quizData.createdQuiz)
+			if (quizData.attemptedQuiz) setAttemptedQuizzes(quizData.attemptedQuiz)
+			setLoading(false)
+		}
+		if (user) fetchQuizData()
+	}, [user])
 
-    const editQuizHandle = async (title, questions, isOpen) => {
-        if (!title) setEditQuiz([])
-        else {
-            setLoading(true)
-            console.dir({
-                quizId: createdQuizzes[editQuiz]._id,
-                uid: user.uid,
-                title,
-                questions,
-                isOpen
-            })
-            const results = await fetch('/API/quizzes/edit', {
-                method: 'POST',
-                body: JSON.stringify({
-                    quizId: createdQuizzes[editQuiz]._id,
-                    uid: user.uid,
-                    title,
-                    questions,
-                    isOpen
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            const submitData = await results.json()
-            console.dir(submitData)
-            const temp = [...createdQuizzes]
-            temp[editQuiz[0]].title = title
-            temp[editQuiz[0]].questions = questions
-            temp[editQuiz[0]].isOpen = isOpen
-            setCreatedQuizzes(temp)
-            setEditQuiz([])
-            setLoading(false)
-        }
-    }
+	const editQuizHandle = async (title, questions, isOpen) => {
+		if (!title) setEditQuiz([])
+		else {
+			setLoading(true)
+			console.dir({
+				quizId: createdQuizzes[editQuiz]._id,
+				uid: user.uid,
+				title,
+				questions,
+				isOpen,
+			})
+			const results = await fetch('/API/quizzes/edit', {
+				method: 'POST',
+				body: JSON.stringify({
+					quizId: createdQuizzes[editQuiz]._id,
+					uid: user.uid,
+					title,
+					questions,
+					isOpen,
+				}),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			const submitData = await results.json()
+			console.dir(submitData)
+			const temp = [...createdQuizzes]
+			temp[editQuiz[0]].title = title
+			temp[editQuiz[0]].questions = questions
+			temp[editQuiz[0]].isOpen = isOpen
+			setCreatedQuizzes(temp)
+			setEditQuiz([])
+			setLoading(false)
+		}
+	}
 
-    if (loading) return <LoadingScreen />
+	if (loading) return <LoadingScreen />
 
-    if (editQuiz.length)
-        return (
-            <CreateQuiz
-                user={user}
-                quizTitle={createdQuizzes[editQuiz].title}
-                questions={createdQuizzes[editQuiz].questions}
-                responses={createdQuizzes[editQuiz].responses}
-                isOpen={createdQuizzes[editQuiz].isOpen}
-                editQuizHandle={editQuizHandle}
-            />
-        )
-    return (
-        <div className="dash-body">
-            <div className="quizzes">
-                <div className="heading">
-                    <div className="line-left" />
-                    <h2>Created </h2>
-                    <div className="line" />
-                </div>
-                <div className="card-holder">
-                    {createdQuizzes.map((quiz, key) => (
-                        <Link
-                            to={`/responses/${quiz._id}`}
-                            style={{ textDecoration: 'none' }}
-                        >
-                            <CreatedQuizCard
-                                key={key}
-                                index={key}
-                                setEditQuiz={setEditQuiz}
-                                title={quiz.title}
-                                code={quiz._id}
-                                responses={quiz.responses.length}
-                                questions={quiz.questions.length}
-                                isOpen={quiz.isOpen}
-                            />
-                        </Link>
-                    ))}
-                </div>
-            </div>
-            <div className="quizzes">
-                <div className="heading">
-                    <div className="line-left" />
-                    <h2>Attempted </h2>
-                    <div className="line" />
-                </div>
-                <div className="card-holder">
-                    {attemptedQuizzes.map((quiz, key) => (
-                        <JoinedQuizCard
-                            key={key}
-                            title={quiz.title}
-                            score={quiz.responses[0].score}
-                            questions={quiz.totalQuestions}
-                        />
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
+	if (editQuiz.length)
+		return (
+			<CreateQuiz
+				user={user}
+				quizTitle={createdQuizzes[editQuiz].title}
+				questions={createdQuizzes[editQuiz].questions}
+				responses={createdQuizzes[editQuiz].responses}
+				isOpen={createdQuizzes[editQuiz].isOpen}
+				editQuizHandle={editQuizHandle}
+			/>
+		)
+	return (
+		<div className='dash-body'>
+			<div className='quizzes'>
+				<div className='heading'>
+					<div className='line-left' />
+					<h2>Created </h2>
+					<div className='line' />
+				</div>
+				<div className='card-holder'>
+					{createdQuizzes.map((quiz, key) => (
+						<CreatedQuizCard
+							key={key}
+							index={key}
+							setEditQuiz={setEditQuiz}
+							title={quiz.title}
+							code={quiz._id}
+							responses={quiz.responses.length}
+							questions={quiz.questions.length}
+							isOpen={quiz.isOpen}
+						/>
+					))}
+				</div>
+			</div>
+			<div className='quizzes'>
+				<div className='heading'>
+					<div className='line-left' />
+					<h2>Attempted </h2>
+					<div className='line' />
+				</div>
+				<div className='card-holder'>
+					{attemptedQuizzes.map((quiz, key) => (
+						<JoinedQuizCard
+							key={key}
+							title={quiz.title}
+							score={quiz.responses[0].score}
+							questions={quiz.totalQuestions}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default UserDashboard
