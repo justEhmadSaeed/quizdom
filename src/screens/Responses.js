707b012b8200c34f50ece029cd320775c9bba7cc
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-03-02 01:26:13 +0500

    📦 NEW: Show responses table

--- src/screens/Responses.js
+++ src/screens/Responses.js
@@ -6,6 +6,7 @@ const Responses = ({ match }) => {
 	const quizId = match.params.quizCode
 	const uid = firebase.auth().currentUser.uid
 	const [loading, setLoading] = useState(true)
+	const [responses, setResponses] = useState([])
 
 	useEffect(() => {
 		const getResponses = async () => {
@@ -16,14 +17,39 @@ const Responses = ({ match }) => {
 					'Content-Type': 'application/json',
 				},
 			})
-			const responses = await res.json()
+			const result = await res.json()
+			console.log(result)
+			setResponses(result.finalResponse)
 			setLoading(false)
 		}
 		getResponses()
 	}, [quizId, uid])
-
+	console.log(responses)
 	if (loading) return <LoadingScreen />
-	else return <div></div>
+	else
+		return (
+			<div className="margin-top">
+				<h2>Responses</h2>
+				<table className="response-table">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Email Address</th>
+							<th>Score</th>
+						</tr>
+					</thead>
+					<tbody>
+						{responses.map((resp, key) => (
+							<tr key={key}>
+								<td>{resp.name}</td>
+								<td>{resp.email}</td>
+								<td>{resp.score}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		)
 }
 
 export default Responses
